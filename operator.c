2a1182daea6a065f0d3e4563d5d6f57aaabe58c6
gunjit <73329409+GUNJITDALAL@users.noreply.github.com>
2021-02-01 19:47:11 +0530

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int x=10;
+    int temp;
+    temp=--x;
+    printf("value of x=%d",x);
+    printf("\nvalue of temp=%d",temp);
+    printf("\nValue of x=%d",x);
+    return 0;
+}
